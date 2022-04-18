9f17a6f0beec6ec96e9b4f7688cdd5bbd13f1f95
JavaYuhm <kora47461381@gmail.com>
2022-04-19 00:44:15 +0900

    2022-04-18 과제 Reactor REST API commit

--- src/main/java/com/codestates/assignment1/hello/RestWebFluxHandler.java
+++ src/main/java/com/codestates/assignment1/hello/RestWebFluxHandler.java
@@ -7,11 +7,18 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
 @Component
 public class RestWebFluxHandler {
     public Mono<ServerResponse> hello(ServerRequest request) {
 
-        String name = request.queryParam("name").get();
+        Optional<String> queryString =  request.queryParam("name");
+        if(!queryString.isPresent()){
+            return ServerResponse.badRequest().build();
+        }
+
+        String name = queryString.get();
         RestWebFlux greeting = new RestWebFlux(name, "hello! " + name);
 
         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
