84c64cd7f48458a9d92adf52dbcc9a1af2bb5f9f
JavaYuhm <99159721+JavaYuhm@users.noreply.github.com>
2022-04-19 16:30:57 +0900

    Update RestWebFluxHandler.java

--- src/main/java/com/codestates/assignment1/hello/RestWebFluxHandler.java
+++ src/main/java/com/codestates/assignment1/hello/RestWebFluxHandler.java
@@ -11,6 +11,8 @@ import java.util.Optional;
 
 @Component
 public class RestWebFluxHandler {
+    // router에서 넘겨준  ServerRequest 객체를 받음.
+    // bodyToMono() 메소드를 통해 json 데이터를 Dto로 매핑 가능하다고 하는데, Test 해볼 것,,
     public Mono<ServerResponse> hello(ServerRequest request) {
 
         Optional<String> queryString =  request.queryParam("name");
